a64310364d10f33cf667467ce7005342d4ffb8ce
Alex Skorulis <skorulis@gmail.com>
2020-12-31 17:47:15 +1100

    Improve API and readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# node_google_trends
+
+Simple express wrapper around the https://www.npmjs.com/package/google-trends-api to expose to other services. Does not expose all possible methods and parameters, only  those required for other projects.
+
--- index.js
+++ index.js
@@ -5,14 +5,26 @@ const app = express()
 app.set('json spaces', 2)
 
 async function daily(country) {
-    let result = await googleTrends.dailyTrends({geo: country})
+    let result = await googleTrends.dailyTrends({geo: country, timezone: 0})
     return result;
 }
 
 app.get("/daily/:country", async (req, res) => {
-    let result = await daily(req.params.country);
     res.type('json');
-    res.send(result)
+
+    try {
+        let resultBody = await daily(req.params.country);
+        //console.log(`Got body ${resultBody}`)
+        let result = JSON.parse(resultBody)
+        
+        res.send(result.default)
+    } catch(err) {
+        let message = `${err}`
+        let errorPayload = {message: message}
+        res.status(400)
+        res.send(errorPayload)
+    }
+    
 });
 
 app.listen(5001)
